1168e8e02858ae1ec389ff3e37df7fceed54bdff
JustLuke99 <60072755+JustLuke99@users.noreply.github.com>
2024-02-04 18:28:15 +0100

    Added tmp functions

--- perun/select/better_repository_selection.py
+++ perun/select/better_repository_selection.py
@@ -12,6 +12,7 @@ from perun.select.abstract_base_selection import AbstractBaseSelection
 from perun.utils.structs import MinorVersion
 from perun.logic.stats import get_stats_of
 
+
 class BetterRepositorySelection(AbstractBaseSelection):
     # nastavení soubor/složka(rek)/projekt -checkby file/folder/project
     # nastevní thresholdu - hodnota/hodnoty/dynamicky podle průměru (musí být celý prjekt/posledních x commitů) -treshhold num/nums/dynamic
@@ -26,15 +27,12 @@ class BetterRepositorySelection(AbstractBaseSelection):
 
         parents = vcs.walk_minor_versions(MinorVersion.HASH)
 
-
         # NOTE this should be checked in func get_stats_of()
         # # TODO update to better if
         # if indicator_data is None:
         #     raise Exception(f"No data (stats) available for version {MinorVersion.HASH}")
 
-
-
-
+        # TODO udělat hledání a počítání souborů ve složkách podle parserů - udělat parsery v configu nebo tak nějak :)
 
         return True, 1
 
@@ -56,3 +54,17 @@ class BetterRepositorySelection(AbstractBaseSelection):
         """
         return True, 1
 
+    def _find_all_by_file_path(data_list, file_path_to_find):
+        return [item for item in data_list if file_path_to_find in item.get("file_path")]
+
+
+from typing import List
+
+
+def _find_all_paths(data_list: List[dict]) -> List[str]:
+    paths = []
+    for data in data_list:
+        if not (new_path := data["file_path"].rsplit("/", 1)[0]) in paths:
+            paths.append(new_path)
+
+    return paths
