0834d7297574d3b684fed01d5c2a63669ec466ff
JustLuke99 <60072755+JustLuke99@users.noreply.github.com>
2024-02-20 21:01:25 +0100

    Updated Rule

--- perun/select/rule_kit/rule.py
+++ perun/select/rule_kit/rule.py
@@ -15,7 +15,22 @@ RULE_CONFIG = {
     },
 }
 
+threshold_functions = {
+    "from": lambda value, threshold: value > threshold,
+    "to": lambda value, threshold: value < threshold,
+    "between": lambda value, from_threshold, to_threshold: from_threshold < value < to_threshold
+}
 
 class Rule:
     def check_rule(self, key, value, parser_name) -> bool:
-        ...
+        if parser_name not in RULE_CONFIG.keys():
+            return
+
+        if not RULE_CONFIG[parser_name]["active"]:
+            return
+
+        if key not in RULE_CONFIG[parser_name]["rules"].keys():
+            return
+
+        for rule in RULE_CONFIG[parser_name]["rules"][key]:
+            threshold_functions[rule["threshold_type"]](value, rule["from"])
